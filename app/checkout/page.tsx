1d7920249cf12db92597cc6289f09e0780c521b6
Adithya <adithya.vardhan1617@gmail.com>
2025-07-14 19:25:04 +0530

    checkout page renders dynamically

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+export const dynamic = "force-dynamic";
 
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
 import { Badge } from '@/components/ui/badge';
